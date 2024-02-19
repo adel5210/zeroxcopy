7a26cddbbd0b0d6d0f2508699b0147e6a4233aeb
adel <adel_5210m@yahoo.com>
2024-02-19 15:00:22 +0800

    Minor fix compile

--- src/main/java/com/adel/RunZC.java
+++ src/main/java/com/adel/RunZC.java
@@ -1,7 +1,14 @@
 package com.adel;
 
-import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
 
+/**
+ * Compile JAR with
+ * mvn clean compile package
+ */
 public class RunZC {
     public static void main(String[] args) {
         System.out.println("--- ZeroCopy initialized ...");
@@ -10,8 +17,23 @@ public class RunZC {
             System.out.println("usage: RunZC <source> <destination> \n");
             return;
         }
-        try(final ZeroCopyChannel zcc = new ZeroCopyChannel(args[0], args[1])){
+        final String fromFile = args[0];
+        final String toFile = args[1];
+        try (final ZeroCopyChannel zcc = new ZeroCopyChannel(fromFile, toFile)) {
             System.out.println("--- ZeroCopy Success...");
+
+            //Generate checksum
+            byte[] data = Files.readAllBytes(Paths.get(fromFile));
+            byte[] hash = MessageDigest.getInstance("MD5").digest(data);
+            final String fromCheckSum = new BigInteger(1, hash).toString(16);
+
+            data = Files.readAllBytes(Paths.get(toFile));
+            hash = MessageDigest.getInstance("MD5").digest(data);
+            final String toCheckSum = new BigInteger(1, hash).toString(16);
+
+            System.out.println("fromFile: " + fromCheckSum);
+            System.out.println("toFile:   " + toCheckSum);
+            System.out.println(fromCheckSum.equals(toCheckSum) ? "Valid" : "Invalid");
         } catch (Exception e) {
             e.printStackTrace();
         }
