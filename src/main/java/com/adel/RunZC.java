e612b17b36395d2bff96b5a3cc02f010d03306f5
adel <adel_5210m@yahoo.com>
2024-02-19 14:46:03 +0800

    Minor fix compile

--- src/main/java/com/adel/RunZC.java
+++ src/main/java/com/adel/RunZC.java
@@ -1,17 +1,23 @@
 package com.adel;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class RunZC {
+    public static void main(String[] args) {
+        System.out.println("--- ZeroCopy initialized ...");
+        final long startTime = System.currentTimeMillis();
+        try {
+            if (args.length < 2) {
+                System.out.println("usage: RunZC <source> <destination> \n");
+                return;
+            }
+
+            new ZeroCopyChannel().process(args[0], args[1]);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        System.out.println("--- ZeroCopy process time(ms): " + (System.currentTimeMillis() - startTime));
     }
 }
--- src/main/java/com/adel/ZeroCopyChannel.java
+++ src/main/java/com/adel/ZeroCopyChannel.java
@@ -1,2 +1,22 @@
-package com.adel;public class ZeroCopyChannel {
+package com.adel;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+
+public class ZeroCopyChannel {
+    protected void process(final String from,
+                           final String to) throws IOException {
+        FileChannel source = null;
+        FileChannel dest = null;
+        try {
+            source = new FileInputStream(from).getChannel();
+            dest = new FileOutputStream(to).getChannel();
+            source.transferTo(0, source.size(), dest);
+        } finally {
+            if (null != source) source.close();
+            if (null != dest) dest.close();
+        }
+    }
 }
