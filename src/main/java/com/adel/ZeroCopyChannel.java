bf9a0651a800e6d21f2919e99523678477ca2218
adel <adel_5210m@yahoo.com>
2024-02-20 11:19:17 +0800

    File transfer display

--- src/main/java/com/adel/ZeroCopyChannel.java
+++ src/main/java/com/adel/ZeroCopyChannel.java
@@ -5,23 +5,27 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ZeroCopyChannel implements AutoCloseable {
 
     private final FileChannel source;
     private final FileChannel dest;
+    private final ExecutorService displayService;
 
     public ZeroCopyChannel(final String fromFile, final String toFile) throws IOException {
         this.source = new FileInputStream(fromFile).getChannel();
         this.dest = new FileOutputStream(toFile).getChannel();
 
+        displayService = Executors.newVirtualThreadPerTaskExecutor();
         CompletableFuture.runAsync(() -> {
             while (true) {
                 try {
 
-                    System.out.println("File transferred: " +
-                            ((dest.size() / source.size()) * 100) + "%");
+                    double ratio = (double) dest.size() / source.size();
+                    int percent = (int) (ratio * 100);
+                    System.out.println("File transferred: " + percent + "% ");
 
                     if (source.size() == dest.size()) break;
 
@@ -30,7 +34,7 @@ public class ZeroCopyChannel implements AutoCloseable {
                     throw new RuntimeException(e);
                 }
             }
-        }, Executors.newVirtualThreadPerTaskExecutor());
+        }, displayService);
 
         source.transferTo(0, source.size(), dest);
     }
@@ -39,5 +43,7 @@ public class ZeroCopyChannel implements AutoCloseable {
     public void close() throws Exception {
         if (null != source) source.close();
         if (null != dest) dest.close();
+        if (null != displayService) displayService.close();
+
     }
 }
